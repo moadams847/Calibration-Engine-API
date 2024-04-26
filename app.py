d69c15037909465dbf1bdce581069542cf66c182
moadams847 <moadams847@gmail.com>
2024-04-26 11:42:24 +0000

    updated codebase for deployment

--- app.py
+++ app.py
@@ -5,64 +5,66 @@ import pickle
 import gzip
 import joblib
 
-# app = Flask(__name__)
-
-# def decompress_pickle_gzip(file_path):
-#     with gzip.open(file_path, 'rb') as f:
-#         return pickle.load(f)
-
-# model = decompress_pickle_gzip('assets/model.pkl.gz')
-# model = joblib.load(model)
-
-# # # Load the model once, when the app starts
-# # model = load_model('assets/correction_factor_random_forest_sensor960-25-April-2024')
-
-# @app.route('/')
-# def index():
-#     return 'Welcome to the Calibration Engine API!'
-
-# @app.route('/calibration-engine-api/PM25/v1/', methods=['GET', 'POST'])
-# # @app.route('/PM25/v1/', methods=['GET', 'POST'])
-# def predict_datapoints():
-#     if request.method == 'GET':
-#         return jsonify({'Instruction': 'Send JSON data with Hum, Temp, and PM2_5 for calibration'})
-
-#     # POST: Handle calibration prediction
-#     try:
-#         # Ensure there is JSON data and it is the correct format
-#         json_data = request.json
-#         if not json_data:
-#             return jsonify({'error': 'No JSON data provided'}), 400
-
-#         # Create DataFrame from JSON, handle potential errors in key names
-#         try:
-#             json_to_df = pd.read_json(json_data)
-#             df_for_calibration = json_to_df[['Hum', 'Temp', 'PM2_5']]
-#         except KeyError as e:
-#             return jsonify({'error': f'Missing columns in the data: {str(e)}'}), 400
-
-#         # Make predictions
-#         # predictions = predict_model(model, data=df_for_calibration)
-#         # predictions.rename(columns={'prediction_label': 'calibrated_PM2_5'}, inplace=True)
-
-#         predictions_array = model.predict(df_for_calibration)
-#         predictions = pd.DataFrame(predictions_array, columns=['calibrated_PM2_5'], index = json_to_df.index)
-
-#         # Combine predictions with original data
-#         combined_df = pd.concat([json_to_df, predictions['calibrated_PM2_5']], axis=1)
-#         return combined_df.to_json(orient='records')
-
-#     except Exception as e:
-#         return jsonify({'error': f'An error occurred: {str(e)}'}), 500
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=False)
-
-from flask import Flask
 app = Flask(__name__)
 
+def decompress_pickle_gzip(file_path):
+    with gzip.open(file_path, 'rb') as f:
+        return pickle.load(f)
+
+model = decompress_pickle_gzip('assets/model.pkl.gz')
+model = joblib.load(model)
+
+# # Load the model once, when the app starts
+# model = load_model('assets/correction_factor_random_forest_sensor960-25-April-2024')
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Welcome to the Calibration Engine API!'
+
+@app.route('/calibration-engine-api/PM25/v1/', methods=['GET', 'POST'])
+# @app.route('/PM25/v1/', methods=['GET', 'POST'])
+def predict_datapoints():
+    if request.method == 'GET':
+        return jsonify({'Instruction': 'Send JSON data with Hum, Temp, and PM2_5 for calibration'})
+
+    # POST: Handle calibration prediction
+    try:
+        # Ensure there is JSON data and it is the correct format
+        json_data = request.json
+        if not json_data:
+            return jsonify({'error': 'No JSON data provided'}), 400
+
+        # Create DataFrame from JSON, handle potential errors in key names
+        try:
+            json_to_df = pd.read_json(json_data)
+            df_for_calibration = json_to_df[['Hum', 'Temp', 'PM2_5']]
+        except KeyError as e:
+            return jsonify({'error': f'Missing columns in the data: {str(e)}'}), 400
+
+        # Make predictions
+        # predictions = predict_model(model, data=df_for_calibration)
+        # predictions.rename(columns={'prediction_label': 'calibrated_PM2_5'}, inplace=True)
+
+        predictions_array = model.predict(df_for_calibration)
+        predictions = pd.DataFrame(predictions_array, columns=['calibrated_PM2_5'], index = json_to_df.index)
+
+        # Combine predictions with original data
+        combined_df = pd.concat([json_to_df, predictions['calibrated_PM2_5']], axis=1)
+        return combined_df.to_json(orient='records')
+
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False)
+
+
+#test 
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
 
