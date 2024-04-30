30a74ee4e93fc2e3be6aac4f0581e78525caefff
moadams847 <moadams847@gmail.com>
2024-04-30 15:56:05 +0000

    test simple model for deployment

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ import joblib
 
 app = Flask(__name__)
 
-def decompress_pickle_gzip(file_path):
-    with gzip.open(file_path, 'rb') as f:
-        return pickle.load(f)
-
-model = decompress_pickle_gzip('assets/model.pkl.gz')
-model = joblib.load(model)
-
 # # Load the model once, when the app starts
 # model = load_model('assets/correction_factor_random_forest_sensor960-25-April-2024')
 
@@ -37,7 +30,7 @@ def predict_datapoints():
         # Create DataFrame from JSON, handle potential errors in key names
         try:
             json_to_df = pd.read_json(json_data)
-            df_for_calibration = json_to_df[['Hum', 'Temp', 'PM2_5']]
+            # df_for_calibration = json_to_df[['Hum', 'Temp', 'PM2_5']]
         except KeyError as e:
             return jsonify({'error': f'Missing columns in the data: {str(e)}'}), 400
 
@@ -45,19 +38,23 @@ def predict_datapoints():
         # predictions = predict_model(model, data=df_for_calibration)
         # predictions.rename(columns={'prediction_label': 'calibrated_PM2_5'}, inplace=True)
 
-        predictions_array = model.predict(df_for_calibration)
-        predictions = pd.DataFrame(predictions_array, columns=['calibrated_PM2_5'], index = json_to_df.index)
+        # Apply the formula to calculate Corrected PM2_5
+        print(json_to_df)
+        json_to_df['calibrated_PM2_5'] = 332.38 + (2.41 * json_to_df['PM2_5']) + (-6.66 * json_to_df['Temp']) + (-1.55 * json_to_df['Hum'])
+
+        # predictions_array = model.predict(df_for_calibration)
+        # predictions = pd.DataFrame(predictions_array, columns=['calibrated_PM2_5'], index = json_to_df.index)
 
         # Combine predictions with original data
-        combined_df = pd.concat([json_to_df, predictions['calibrated_PM2_5']], axis=1)
-        return combined_df.to_json(orient='records')
+        # combined_df = pd.concat([json_to_df, predictions['calibrated_PM2_5']], axis=1)
+        return json_to_df.to_json(orient='records')
 
     except Exception as e:
         return jsonify({'error': f'An error occurred: {str(e)}'}), 500
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=False)
 
 
 #test 
